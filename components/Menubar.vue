6f091aaf8fa564a7a3660147f823f07f4aef190f
Petr <piotr.makarov@gmail.com>
2023-02-21 21:58:33 +0500

    Rewrite menubar resizing with @media

--- components/Menubar.vue
+++ components/Menubar.vue
@@ -7,7 +7,6 @@
       <img src="/logo.png" height="30">
     </nuxt-link>
     <div
-      v-if="mobile"
       class="hamburger-menu-button"
       @click="showMenu = !showMenu"
     >
@@ -16,7 +15,7 @@
     <div
       class="menu-items"
       ref="menuItems"
-      :class="{ mobile, shown: mobile && showMenu }"
+      :class="{ shown: showMenu }"
     >
       <a href="/#services">Услуги</a>
       <a href="/#portfolio">Портфолио</a>
@@ -46,40 +45,47 @@
   transform-style: preserve-3d;
 }
 
-.menu-items.mobile {
-  display: flex;
-  flex-direction: column;
-  gap: 1em;
-  padding: 1em;
-  border-radius: 0 0 20px 20px;
-
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: var(--menu-height);
-  transform: translateY(calc(-100% - var(--menu-height))) translateZ(-10px);
-
-  background: white;
-  box-shadow: 0px -20px 0px 0px white;
-  transition: .3s;
-}
-
-.menu-items.mobile.shown {
-  transform: none;
-}
-
 .hamburger-menu-button {
+  display: none;
   font-size: 1.5em;
   cursor: pointer;
+}
+
+.menu-items a {
+  margin: 0 10px;
+}
+
+@media all and (max-width: 710px) {
+  .menu-items {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    padding: 1em;
+    border-radius: 0 0 20px 20px;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: var(--menu-height);
+    transform: translateY(calc(-100% - var(--menu-height))) translateZ(-10px);
+
+    background: white;
+    box-shadow: 0px -20px 0px 0px white;
+    transition: .3s;
+  }
+
+  .menu-items.shown {
+    transform: none;
+  }
+
+  .hamburger-menu-button {
+    display: block;
+  }
 }
 
 a {
   font-family: Montserrat;
   font-weight: 500;
-}
-
-.menu-items:not(.mobile) a {
-  margin: 0 10px;
 }
 
 a#become-client {
@@ -92,18 +98,10 @@ import { mapMutations } from 'vuex';
 
 export default {
   data: () => ({
-    mobile: false,
     showMenu: false,
   }),
   methods: {
-    onResize() {
-      this.mobile = window.innerWidth <= 710;
-    },
     ...mapMutations(['setShowOrder']),
   },
-  mounted() {
-    this.onResize();
-    window.addEventListener('resize', this.onResize);
-  }
 };
 </script>
